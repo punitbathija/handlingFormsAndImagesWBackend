890087f42f03e7944e42c161b744cf2a4aebd065
Punit Bathija <punit644@gmail.com>
2023-02-08 15:09:53 +0530

    Learnt about request's and response using urlencoded

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: "true" }));
 
 app.get("/myget", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  console.log(req.body);
+  res.send(req.body);
 });
 
 module.exports = app;
