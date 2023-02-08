cf2820baa8996dd8265afb6812b3cde9f4e51add
Punit Bathija <punit644@gmail.com>
2023-02-08 16:28:57 +0530

    Added a mrthod onto POST form to handle images on frontend and backend

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const fileUpload = require("express-fileupload");
 app.use(express.json());
 app.use(express.urlencoded({ extended: "true" }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/temp/",
+  })
+);
+
 app.set("view engine", "ejs");
 
 app.get("/myget", (req, res) => {
   console.log(req.body);
   res.send(req.query);
+});
+
+app.post("/mypost", (req, res) => {
+  console.log(req.files);
+  res.send(req.body);
 });
 
 app.get("/getForm", (req, res) => {
