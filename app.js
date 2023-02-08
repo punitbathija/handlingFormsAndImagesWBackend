c93e43623c5bdaf1ec37bd3e636b10a3d99d807d
Punit Bathija <punit644@gmail.com>
2023-02-08 15:48:37 +0530

    Added EJS templates for form handling

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: "true" }));
+app.set("view engine", "ejs");
 
 app.get("/myget", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+  res.send("<h1>Welcome to My App</h1>");
+});
+
+app.get("/getForm", (req, res) => {
+  res.render("getForm");
+});
+
+app.get("/postForm", (req, res) => {
+  res.render("postForm");
 });
 
 module.exports = app;
